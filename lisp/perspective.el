d5d79d9dee234c940de46284586cfab277f99eb3
Ian S. Pringle <ian@dapringles.com>
2026-02-04 10:10:46 -0600

    added perspective.el
    this will need to be updated when melpa version updates, as it won't need the
    persp-names overwrite, that's been upstreamed

--- lisp/perspective.el
+++ lisp/perspective.el
@@ -11,15 +11,38 @@
   :init
   (setq persp-mode-prefix-key (kbd "C-z"))
   :custom
-  (persp-sort 'created)
+  (persp-sort 'oldest)
   (persp-state-default-file
    (expand-file-name "persp-state" minimal-emacs-user-directory))
   :hook
   (elpaca-after-init . persp-mode)
   (kill-emacs . persp-state-save)
   :config
+  (defun persp-names ()
+    "Return a list of the names of all perspectives on the `selected-frame'."
+    (let ((persps (hash-table-values (perspectives-hash))))
+      (cond ((eq persp-sort 'name)
+             (sort (mapcar 'persp-name persps) 'string<))
+            ((eq persp-sort 'access)
+             (mapcar 'persp-name
+                     (sort persps
+                           (lambda (a b)
+                             (time-less-p (persp-last-switch-time b)
+                                          (persp-last-switch-time a))))))
+            ((eq persp-sort 'created)
+             (mapcar 'persp-name
+                     (sort persps
+                           (lambda (a b)
+                             (time-less-p (persp-created-time b)
+                                          (persp-created-time a))))))
+            ((eq persp-sort 'oldest)
+             (mapcar 'persp-name
+                     (sort persps
+                           (lambda (a b)
+                             (time-less-p (persp-created-time a)
+                                          (persp-created-time b)))))))))
   (with-eval-after-load 'consult
-    (consult-customize consult--source-buffer :hidden t :default nil)
+    (consult-customize consult-source-buffer :hidden t :default nil)
     (add-to-list 'consult-buffer-sources persp-consult-source)))
 
 ;;; perspective.el ends here
