b16c53964c04869be397d49550bf2b467829857a
Ian Pringle <ian@dapringles.com>
2026-02-18 16:08:18 -0600

    Refactor Lit template support
    Replace polymode implementation with template-literals-ts-mode and lit-ts-mode using tree-sitter for better performance and simpler configuration.

--- lisp/lit.el
+++ lisp/lit.el
@@ -11,84 +11,12 @@
 (setq css-indent-offset 2)
 (setq sgml-basic-offset 2)
 
-;; Use polymode for multi-language highlighting in template literals
-(use-package polymode
+(use-package template-literals-ts-mode
   :ensure t
-  :config
-  (setq polymode-prefix-key (kbd "C-c m"))
+  :hook ((js-ts-mode . template-literals-ts-mode)
+         (typescript-ts-mode . template-literals-ts-mode)))
 
-  ;; Use tree-sitter modes as host
-  (define-hostmode poly-lit-typescript-hostmode
-    :mode 'typescript-ts-mode)
-
-  (define-hostmode poly-lit-javascript-hostmode
-    :mode 'js-ts-mode)
-
-  ;; Define HTML template literal as inner mode (use html-ts-mode)
-  (define-innermode poly-lit-html-template-innermode
-    :mode 'html-ts-mode
-    :head-matcher "html\\s-*`"
-    :tail-matcher "`"
-    :head-mode 'host
-    :tail-mode 'host
-    :fallback-mode 'host
-    :allow-nested t)
-
-  ;; Define CSS template literal as inner mode
-  (define-innermode poly-lit-css-template-innermode
-    :mode 'css-ts-mode
-    :head-matcher "css\\s-*`"
-    :tail-matcher "`"
-    :head-mode 'host
-    :tail-mode 'host
-    :fallback-mode 'host)
-
-  ;; Define JavaScript expressions inside HTML templates (${...})
-  (define-innermode poly-lit-js-expression-innermode
-    :mode 'js-ts-mode
-    :head-matcher "\\${[ \t\n]*"
-    :tail-matcher "[ \t\n]*}"
-    :head-mode 'host
-    :tail-mode 'host
-    :protect-font-lock nil
-    :protect-syntax nil)
-
-  ;; Define the polymode for TypeScript + Lit
-  (define-polymode poly-lit-ts-mode
-    :hostmode 'poly-lit-typescript-hostmode
-    :innermodes '(poly-lit-html-template-innermode
-                  poly-lit-css-template-innermode
-                  poly-lit-js-expression-innermode)
-    :lighter " PolyLit")
-
-  ;; Define the polymode for JavaScript + Lit
-  (define-polymode poly-lit-js-mode
-    :hostmode 'poly-lit-javascript-hostmode
-    :innermodes '(poly-lit-html-template-innermode
-                  poly-lit-css-template-innermode
-                  poly-lit-js-expression-innermode)
-    :lighter " PolyLit"))
-
-;; Auto-activate for files with Lit imports
-;; DISABLED: Not working as desired
-;; (defun lit-maybe-enable-polymode ()
-;;   "Enable poly-lit mode if file contains Lit imports."
-;;   (when (and (buffer-file-name)
-;;              (not (bound-and-true-p polymode-mode)))
-;;     (save-excursion
-;;       (goto-char (point-min))
-;;       (when (re-search-forward "from ['\"]lit['\"]\\|@lit/\\|import.*LitElement" nil t)
-;;         (cond
-;;          ((or (derived-mode-p 'typescript-ts-mode)
-;;               (derived-mode-p 'typescript-mode))
-;;           (poly-lit-ts-mode))
-;;          ((or (derived-mode-p 'js-ts-mode)
-;;               (derived-mode-p 'js-mode))
-;;           (poly-lit-js-mode)))))))
-
-;; (add-hook 'typescript-ts-mode-hook #'lit-maybe-enable-polymode)
-;; (add-hook 'typescript-mode-hook #'lit-maybe-enable-polymode)
-;; (add-hook 'js-ts-mode-hook #'lit-maybe-enable-polymode)
-;; (add-hook 'js-mode-hook #'lit-maybe-enable-polymode)
+(use-package lit-ts-mode
+  :ensure (:host github :repo "ispringle/lit-ts-mode"))
 
 ;;; lit.el ends here
