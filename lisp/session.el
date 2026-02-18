d91b9df9be0efc47a088938c1099ff65a62b1564
Ian Pringle <ian@dapringles.com>
2026-02-18 16:08:18 -0600

    Add session management features
    Add tab-bar mode with M-[/M-] navigation, easysession for automatic session saving, and enhanced project switching commands including Magit integration. Fix formatting and diminish.

--- lisp/session.el
+++ lisp/session.el
@@ -6,7 +6,7 @@
 
 (use-package autorevert
   :ensure nil
-  :diminish auto-revert-mode
+  :diminish
   :commands (auto-revert-mode global-auto-revert-mode)
   :hook (elpaca-after-init . global-auto-revert-mode)
   :custom
@@ -41,7 +41,7 @@
   (savehist-autosave-interval 600)
   (savehist-additional-variables
    '(kill-ring register-alist mark-ring global-mark-ring
-     search-ring regexp-search-ring)))
+               search-ring regexp-search-ring)))
 
 (use-package saveplace
   :ensure nil
@@ -49,5 +49,45 @@
   :hook (elpaca-after-init . save-place-mode)
   :custom
   (save-place-limit 400))
+
+(use-package tab-bar
+  :ensure nil
+  :hook (elpaca-after-init . tab-bar-mode)
+  :general
+  ("M-[" #'tab-bar-switch-to-prev-tab
+   "M-]" #'tab-bar-switch-to-next-tab
+   "C-x t t" #'tab-bar-switch-to-tab)
+  :custom
+  (tab-bar-show nil)
+  (tab-bar-new-tab-choice "*scratch*")
+  (tab-bar-close-button-show nil)
+  (tab-bar-new-button-show nil))
+
+(use-package project
+  :ensure nil
+  :custom
+  (project-switch-use-entire-map nil)
+  (project-switch-commands
+   '((project-find-file "Find file")
+     (project-find-dir "Find dir")
+     (project-dired "Dired")
+     (magit-project-status "Magit" ?v)
+     (project-eshell "Eshell")
+     (project-shell "Shell")
+     (project-find-regexp "Grep"))))
+
+(use-package easysession
+  :ensure t
+  :diminish easysession-save-mode
+  :custom
+  (easysession-save-interval (* 10 60))
+  :init
+  (add-hook 'elpaca-after-init-hook #'easysession-save-mode 103)
+  :config
+  (with-eval-after-load 'easysession
+    (require 'easysession-scratch)
+    (require 'easysession-magit)
+    (easysession-scratch-mode 1)
+    (easysession-magit-mode 1)))
 
 ;;; session.el ends here
