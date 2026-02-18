b11f489fbf693cb218335b9f2db87a61bfb30e75
Ian Pringle <ian@dapringles.com>
2026-02-18 16:08:17 -0600

    Switch from paredit to lispy for elisp editing
    Disable paredit in favor of lispy. Add package-lint, fix formatting, and clean up diminish declarations.

--- lisp/elisp.el
+++ lisp/elisp.el
@@ -7,7 +7,7 @@
 
 (use-package aggressive-indent
   :ensure t
-  :diminish aggressive-indent-mode
+  :diminish
   :commands aggressive-indent-mode
   :hook (emacs-lisp-mode . aggressive-indent-mode))
 
@@ -16,24 +16,35 @@
   :commands highlight-defined-mode
   :hook (emacs-lisp-mode . highlight-defined-mode))
 
-(use-package paredit
+(use-package lispy
   :ensure t
-  :diminish paredit-mode
+  :hook ((lisp-mode emacs-lisp-mode scheme-mode common-lisp-mode) . lispy-mode)
+  :custom
+  (lispy-close-quotes-at-end-p t))
+
+(use-package paredit
+  :disabled
+  :ensure t
   :commands paredit-mode
   :hook (emacs-lisp-mode . paredit-mode)
   :general
   (:keymaps 'paredit-mode-map
-   "RET" nil))
+            "RET" nil))
 
 (use-package page-break-lines
   :ensure t
+  :diminish
   :commands (page-break-lines-mode global-page-break-lines-mode)
   :hook (emacs-lisp-mode . page-break-lines-mode))
 
 (use-package elisp-refs
   :ensure t
   :commands (elisp-refs-function elisp-refs-macro elisp-refs-variable
-             elisp-refs-special elisp-refs-symbol))
+                                 elisp-refs-special elisp-refs-symbol))
+
+(use-package whitespace
+  :ensure nil
+  :diminish)
 
 ;; Line length enforcement
 (add-hook 'emacs-lisp-mode-hook
@@ -42,5 +53,9 @@
             (setq whitespace-line-column 80)
             (setq whitespace-style '(face lines-tail))
             (whitespace-mode 1)))
+
+(use-package package-lint
+  :defer t
+  :commands (package-lint-current-buffer package-lint-buffer))
 
 ;;; elisp.el ends here
